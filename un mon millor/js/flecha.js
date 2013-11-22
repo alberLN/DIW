8d0c88de38dd6e3667082ce48a6ff2638390219d
alberLN <albertolahuertanavarro@gmail.com>
2013-11-22 11:12:58 +0100

    un mundo mejor 1.0
    El historial de subidas se encuentra en archivos .txt

---
+++ un mon millor/js/flecha.js
@@ -0,0 +1,19 @@
+$( "#botonUp" ).css( "display", "none");
+
+$( window ).scroll(function() {
+  if($(window).scrollTop() >= 100)
+  {
+	$( "#botonUp" ).fadeIn(500);
+  }
+  else
+  {
+	$( "#botonUp" ).fadeOut(500);
+  }
+});
+
+function up()
+{
+	$('html, body').animate({scrollTop:0}, 'slow');
+}
+
+//Falta poner fabicon y separador
